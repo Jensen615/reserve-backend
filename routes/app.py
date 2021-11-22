6a7d760a6131d512c6505784f1608967297d5478
brsanchez21 <90644757+brsanchez21@users.noreply.github.com>
2021-11-21 22:30:20 -0500

    changes

--- routes/app.py
+++ routes/app.py
@@ -1,8 +1,11 @@
 from db import db
+import json
+
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
-db_filename = "cms.db"
+db_filename = "reserve.db"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -12,8 +15,14 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+def success_response(data, code=200):
+    return json.dumps(data), code
 
-# your routes here
+def failure_response(data, code=404):
+    return json.dumps({"error": data}), code
+
+
+
 
 
 if __name__ == "__main__":
--- routes/db.py
+++ routes/db.py
@@ -2,4 +2,39 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# your classes here
+class Rooms(db.Model):
+    __tablename__ = "rooms"
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String, nullable=False)
+    room_name = db.Column(db.String, nullable=False)
+    hall = db.Column(db.String, nullable=False)
+    dates = db.Column(db.String, nullable=False)
+    times = db.Column(db.String, nullable=False)
+    features = db.Column(db.Boolean, nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    day_of_week = db.Column(db.String, nullable=False)
+
+
+    def __init__(self, **kwargs):
+        self.code = kwargs.get("code")
+        self.name = kwargs.get("name")
+        self.location = kwargs.get("location")
+        self.room_name = kwargs.get("room_name")
+        self.hall = kwargs.get("hall")
+        self.dates = kwargs.get("dates")
+        self.times = kwargs.get("times")
+        self.features = kwargs.get("features")
+        self.capacity = kwargs.get("capacity")
+        self.image = kwargs.get("image")
+        self.day_of_week = kwargs.get("day_of_week")
+    
+
+    
+
+
+    
+
+
+
+
